e0e9cbd1cfc6087de686d6f2241de963ab50a9fe
Saumil343 <53990452+Saumil343@users.noreply.github.com>
2023-07-14 01:19:08 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,3 @@
-
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-minimal-hvm-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["amazon"]
-}
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
